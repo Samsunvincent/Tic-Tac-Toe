be59c18d53abe87d7c52fd9392f2622fca32fd3c
samsun <samsunvincent143@gmail.com>
2024-10-28 11:35:33 +0530

    first commit

---
+++ tictactoe/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import Square from './Components/Square'
+import Board from './Components/Board'
+
+
+function App() {
+
+
+  return (
+    <>
+     <Square/>
+     <Board/>
+    </>
+  )
+}
+
+export default App
